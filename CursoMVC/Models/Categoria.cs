ec5e84b239427d5ab252674ff3d1e9a08dbbcdcb
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-21 15:42:56 -0300

    Ajusta Visualização dos Proutos

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace CursoMVC.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo descrição é obrigatório")]
+
         public string Descricao { get; set; }
         
         public List<Produto> Produtos { get; set; }
--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace CursoMVC.Models
     {
         public int Id { get; set; }
         
+        [Display(Name = "Descrição")]
+        
+        [Range(1, 10, ErrorMessage = "Valor além do suportado")]
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public int Quantidade { get; set; }
