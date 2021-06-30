2592a8ed4a111465a1fd822a612c6bdef336e4ac
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-30 16:12:17 -0300

    ok

--- CursoMVC/Models/Context.cs
+++ CursoMVC/Models/Context.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoMVC.Models
 {
     public class Context : DbContext
     {
-        public virtual DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Produto> Produtos { get; set; }
 
-        public Context(DbContextOptions<Context> options) : base(options)
-        { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=tcp:cursomvcdbserverjefdev.database.windows.net,1433;Initial Catalog=CursoMVC_db;User Id=jeferson@cursomvcdbserverjefdev;Password=Ev@92498528");
+        }
 
         public virtual void SetModified(object entity)
         {
